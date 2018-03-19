6ec3590e90ec4ab388657f46db95776c05121ada
vyuldashev <misterio92@gmail.com>
2018-03-20 00:11:14 +0300

    noty notifications

--- src/plugins/notifications/index.js
+++ src/plugins/notifications/index.js
@@ -1,10 +1,25 @@
-import Notify from 'notyf';
-
-const notification = new Notify({
-  delay: 5000
-});
+import Noty from 'noty';
 
 export default {
-    success: message => notification.confirm(message),
-    error: message => notification.alert(message)
+    create(parameters) {
+        parameters.theme = 'bootstrap-v3';
+        parameters.layout = 'topRight';
+
+        return new Noty(parameters);
+    },
+    alert(text) {
+        this.create({type: 'alert', text: text}).show();
+    },
+    success(text) {
+        this.create({type: 'success', text: text).show();
+    },
+    warning(text) {
+        this.create({type: 'warning', text: text}).show();
+    },
+    error(text) {
+        this.create({type: 'error', text: text}).show();
+    },
+    info(text) {
+        this.create({type: 'info', text: text}).show();
+    }
 }
