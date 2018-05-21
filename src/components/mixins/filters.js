2af7c9920edd188cdae7a19bfb173b16e557ce87
vyuldashev <misterio92@gmail.com>
2018-05-22 00:28:22 +0300

    i18n filters

--- src/components/mixins/filters.js
+++ src/components/mixins/filters.js
@@ -1,8 +1,22 @@
 import Vue from 'vue';
+import Vuei18n from 'vue-i18n';
 import moment from 'moment-timezone';
 import typeDate from './type-date';
 import accounting from 'accounting';
-import {settings} from '../../index';
+import {settings} from '../../index.js';
+
+export const i18n = new Vuei18n({
+    locale: () => settings.language,
+    fallbackLocale: 'en',
+    messages: {
+        en: {
+            optional: 'No data',
+        },
+        ru: {
+            optional: 'Нет данных',
+        },
+    }
+});
 
 Vue.filter('datetime', (value, fromTz = 'UTC') => {
     return typeDate(value, true, fromTz);
@@ -24,6 +38,6 @@ Vue.filter('money', (value, precision = 2, currency = '') => {
     return accounting.formatMoney(value, currency, precision, ' ', '.');
 });
 
-Vue.filter('optional', (value, replace = 'Нет данных') => {
+Vue.filter('optional', (value, replace = i18n.t('optional')) => {
     return !value || value.length === 0 ? replace : value;
 });
