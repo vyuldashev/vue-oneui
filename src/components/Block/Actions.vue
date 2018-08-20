8e568fabe8ec9f5eeed44ea0f27a779b5c51d647
dgolubkov <golubkovden@gmail.com>
2018-08-20 15:19:11 +0300

    #master added block actions

---
+++ src/components/Block/Actions.vue
@@ -0,0 +1,38 @@
+<template>
+    <li class="dropdown" v-if="hasAnyVisibleItem">
+        <button type="button" data-toggle="dropdown"><i class="si si-settings"/></button>
+        <ul class="dropdown-menu dropdown-menu-right">
+            <action-item v-for="(item, index) in items" v-bind="item" :key="index"/>
+        </ul>
+    </li>
+</template>
+
+<script>
+    import ActionItem from './ActionItem';
+
+    export default {
+        components: {
+            ActionItem
+        },
+        props: {
+            items: {
+                type: Array,
+                required: true,
+            },
+        },
+        computed: {
+            hasAnyVisibleItem() {
+                return this.items.some(({visible = true}) => this.value(visible));
+            },
+        },
+        methods: {
+            value(value) {
+                return typeof value === 'function' ? value() : value;
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
